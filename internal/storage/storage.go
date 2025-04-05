db7e116d5a4d590d564ad6e9a12a1b5c5d87e892
Максим <makwest5008@gmail.com>
2025-04-05 13:48:05 +0300

    Add method uploading

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,8 +1,19 @@
 package storage
 
+import "github.com/istrel/storage/internal/file"
+
 type Storage struct {
 }
 
 func NewStorage() *Storage {
 	return &Storage{}
+}
+
+func (s *Storage) Upload(filename string, blob []byte) (*file.File, error) {
+	return file.NewFile(filename, blob)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//return newFile, nil
 }
